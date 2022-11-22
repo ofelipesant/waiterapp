8a617678330a0bcb3510358fa044d966f5214601
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-11-21 23:11:51 -0300

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import { Text } from './src/components/Text'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [ isFontsLoaded ] = useFonts({
+		'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+		'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+		'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf')
+	})
+
+	if(!isFontsLoaded){
+		return null
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<Text size={24} weight='600'>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
